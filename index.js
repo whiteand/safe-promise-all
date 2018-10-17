6418b44fbb713d2ec9d4dfef88c3601ef4a4f48d
Andrew Beletskiy <andrewbeletskiy@gmail.com>
2018-10-17 13:45:14 +0300

    Added type checking

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+const isPromise = promise => {
+  return promise
+    && promise.then === 'function'
+    && promise.catch === 'function'
+}
+
+class ExtraError extends TypeError {
+  constructor (message, extra) {
+    super(message)
+    this.extra = extra
+  }
+}
+
 module.exports = function promiseAllSafe (promises) {
+  if (!Array.isArray(promises)) {
+    throw new ExtraError('Promises - must be an array', { promises })
+  }
+
+  if (!promises.every(isPromise)) {
+    const notPromises = promises.filter(p => !isPromise(p))
+    throw new ExtraError('Promises - must contain only a promises', { argument: promises, notPromises })
+  }
+
   const _promiseAll = Promise.all(
     promises.map(
       promise => promise
