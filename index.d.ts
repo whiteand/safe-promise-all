bb4160c9d42954a0474fcb30d350633643d807a4
Andrew.Beletskiy <andrew.beletskiy@adraba.io>
2019-05-14 10:00:34 +0300

    Fixed types

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,9 @@ type SafePromiseAllResult<R,E> = {
   result: R | null,
   error: E | null
 }
-export function safePromiseAll<R = any, E = any>(
+declare function safePromiseAll<R = any, E = any>(
   promises: Promise<R>[],
   unsafePromiseAll?: (promises: Promise<R>[]) => Promise<R[]>
 ): Promise<SafePromiseAllResult<R,E>>
+
+export default safePromiseAll
